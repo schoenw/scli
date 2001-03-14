fe26d5b3e2c95288d109e59037d92eaff02dfd6a
Juergen Schoenwaelder <j.schoenwaelder@jacobs-university.de>
2001-03-14 13:32:32 +0000

    Updated the "show bridge ports" command and added a new "show bridge filter" command (which I could not yet test in practice).

--- scli/bridge.c
+++ scli/bridge.c
@@ -31,6 +31,70 @@
 
 
 static void
+show_port(GString *s, dot1dBasePortEntry_t *dot1dBasePortEntry,
+	  ifEntry_t *ifEntry, ifXEntry_t *ifXEntry)
+{
+    int i;
+    
+    g_string_sprintfa(s, "%5u ", dot1dBasePortEntry->dot1dBasePort);
+    if (dot1dBasePortEntry->dot1dBasePortIfIndex) {
+	g_string_sprintfa(s, "%7u   ",
+			  *dot1dBasePortEntry->dot1dBasePortIfIndex);
+    } else {
+	g_string_sprintfa(s, "%7s   ", "");
+    }
+    if (ifEntry) {
+	fmt_enum(s, 20, if_mib_enums_ifType, ifEntry->ifType);
+    }
+    g_string_append(s, "\n");
+}
+
+
+
+static int
+cmd_bridge_ports(scli_interp_t *interp, int argc, char **argv)
+{
+    dot1dBasePortEntry_t **dot1dBasePortTable = NULL;
+    ifEntry_t **ifTable = NULL;
+    ifXEntry_t **ifXTable = NULL;
+    int i, j;
+    
+    g_return_val_if_fail(interp, SCLI_ERROR);
+
+    if (bridge_mib_get_dot1dBasePortTable(interp->peer, &dot1dBasePortTable)) {
+	return SCLI_ERROR;
+    }
+
+    (void) if_mib_get_ifTable(interp->peer, &ifTable);
+    (void) if_mib_get_ifXTable(interp->peer, &ifXTable);
+    
+    if (dot1dBasePortTable) {
+	g_string_append(interp->result, " Port Interface Type\n");
+	for (i = 0; dot1dBasePortTable[i]; i++) {
+	    if (ifTable && dot1dBasePortTable[i]->dot1dBasePortIfIndex) {
+		for (j = 0; ifTable[j]; j++) {
+		    if (ifTable[j]->ifIndex ==
+			*dot1dBasePortTable[i]->dot1dBasePortIfIndex) {
+			break;
+		    }
+		}
+	    }
+	    show_port(interp->result, dot1dBasePortTable[i],
+		      ifTable[j], ifXTable ? ifXTable[j] : NULL);
+	}
+    }
+
+    if (dot1dBasePortTable)
+	bridge_mib_free_dot1dBasePortTable(dot1dBasePortTable);
+    if (ifTable) if_mib_free_ifTable(ifTable);
+    if (ifXTable) if_mib_free_ifXTable(ifXTable);
+
+    return SCLI_OK;
+}
+
+
+
+static void
 show_forward(GString *s, dot1dTpFdbEntry_t *dot1dTpFdbEntry)
 {
     scli_vendor_t *vendor;
@@ -102,45 +166,59 @@ cmd_bridge_forwarding(scli_interp_t *interp, int argc, char **argv)
 
 
 static void
-show_port(GString *s, dot1dBasePortEntry_t *dot1dBasePortEntry,
-	  ifEntry_t **ifTable)
+show_filter(GString *s, dot1dStaticEntry_t *dot1dStaticEntry)
 {
-    g_string_sprintfa(s, "%5u ", dot1dBasePortEntry->dot1dBasePort);
-    if (dot1dBasePortEntry->dot1dBasePortIfIndex) {
-	g_string_sprintfa(s, "%5u ", *dot1dBasePortEntry->dot1dBasePortIfIndex);
+    int i;
+    
+    if (dot1dStaticEntry->dot1dStaticReceivePort) {
+	g_string_sprintfa(s, "%5d ",
+			  dot1dStaticEntry->dot1dStaticReceivePort);
     } else {
 	g_string_sprintfa(s, "%5s ", "");
     }
+
+    if (dot1dStaticEntry->dot1dStaticAddress) {
+	for (i = 0; i < 6; i++) {
+	    g_string_sprintfa(s, "%s%02x", i ? ":" : "",
+			      dot1dStaticEntry->dot1dStaticAddress[i]);
+	}
+    } else {
+	g_string_sprintfa(s, "--:--:--:--:--:--");
+    }
+
+    if (dot1dStaticEntry->dot1dStaticAllowedToGoTo) {
+	/* xxx */
+    }
+
+    g_string_append(s, " ");
+    fmt_enum(s, 15, bridge_mib_enums_dot1dStaticStatus,
+	     dot1dStaticEntry->dot1dStaticStatus);
+ 
     g_string_append(s, "\n");
 }
 
 
 
 static int
-cmd_bridge_ports(scli_interp_t *interp, int argc, char **argv)
+cmd_bridge_filter(scli_interp_t *interp, int argc, char **argv)
 {
-    dot1dBasePortEntry_t **dot1dBasePortTable = NULL;
-    ifEntry_t **ifTable = NULL;
+    dot1dStaticEntry_t **dot1dStaticTable = NULL;
     int i;
     
     g_return_val_if_fail(interp, SCLI_ERROR);
 
-    if (bridge_mib_get_dot1dBasePortTable(interp->peer, &dot1dBasePortTable)) {
+    if (bridge_mib_get_dot1dStaticTable(interp->peer, &dot1dStaticTable)) {
 	return SCLI_ERROR;
     }
 
-    (void) if_mib_get_ifTable(interp->peer, &ifTable);
-    
-    if (dot1dBasePortTable) {
-	g_string_append(interp->result, " Port Interface\n");
-	for (i = 0; dot1dBasePortTable[i]; i++) {
-	    show_port(interp->result, dot1dBasePortTable[i], ifTable);
+    if (dot1dStaticTable) {
+	for (i = 0; dot1dStaticTable[i]; i++) {
+	    show_filter(interp->result, dot1dStaticTable[i]);
 	}
     }
 
-    if (dot1dBasePortTable)
-	bridge_mib_free_dot1dBasePortTable(dot1dBasePortTable);
-    if (ifTable) if_mib_free_ifTable(ifTable);
+    if (dot1dStaticTable)
+	bridge_mib_free_dot1dStaticTable(dot1dStaticTable);
 
     return SCLI_OK;
 }
@@ -158,6 +236,9 @@ scli_init_bridge_mode(scli_interp_t *interp)
 	{ "show bridge", "forwarding",
 	  "show bridge forwarding data base",
 	  cmd_bridge_forwarding },
+	{ "show bridge", "filter",
+	  "show bridge filtering data base",
+	  cmd_bridge_filter },
 	{ NULL, NULL, NULL, NULL }
     };
     
