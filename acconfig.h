617de877e699e4fa9e0d0a4c37963581947fbb6f
Juergen Schoenwaelder <j.schoenwaelder@jacobs-university.de>
2001-12-06 01:16:26 +0000

    Added acconfig.h to define locally used parameters.

---
+++ acconfig.h
@@ -0,0 +1,27 @@
+/*
+ * acconfig.h --
+ *
+ *	autoheader configuration macros
+ *
+ * Copyright (C) 2001 Juergen Schoenwaelder
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * 
+ * @(#) $Id$
+ */
+
+/* The full pathname of the directory containing scli plugins. */
+#define SCLI_PLUGIN_PATH "/usr/local/lib/scli"
+
